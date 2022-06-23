c68592e061e7f567a2e6649762c4c783df582bf3
xasmall <2624816605@qq.com>
2022-06-23 16:09:17 +0800

    Lab15

---
+++ lab15/src/main/java/com/xasmall/lab15/Lab15Application.java
@@ -0,0 +1,13 @@
+package com.xasmall.lab15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab15Application.class, args);
+    }
+
+}
---
+++ lab15/src/test/java/com/xasmall/lab15/Lab15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xasmall.lab15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
